77bf93d6691cfc70ee2f8b039ca18e861c161b9e
788007 <788007@users.noreply.github.com>
2016-10-28 08:51:34 -0700

    Add files via upload

---
+++ lab1026/ListRunner.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class ListRunner here.
+ * 
+ * @Laurel Woods 
+ * @10/28
+ */
+import java.util.Scanner;
+public class ListRunner
+{
+    public static void main(String[] args){
+        
+        Scanner scr = new Scanner(System.in);
+        int ansNum = 0;
+        String ans = "y";
+        int stuIndex = 0;
+        StudentList studList = new StudentList();
+        while(ansNum != 3 || !ans.equals("quit")){
+            System.out.println("Type a number to choose from the options below:"); 
+            System.out.println("1. Add Student to List");
+            System.out.println("2. Print Student List");
+            System.out.println("3. Quit Program");
+            ansNum = scr.nextInt();
+            if(ansNum == 1){
+                if(stuIndex < 3){
+                    studList.addStu(stuIndex);
+                    stuIndex++;
+                }
+            } else if(ansNum == 2){
+
+            }else if(ansNum == 3){
+                ans = "quit";
+            }
+            if (ansNum != 3){
+                System.out.println("Press 1 to complete another action or press 3 to quit"); 
+                ansNum = scr.nextInt();
+                if(ansNum == 3){
+                    ans = "quit";
+                }else System.out.println("\n" + "**************");
+            }
+        }
+        
+    }
+}
---
+++ lab1026/StudentList.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class StudentList here.
+ * 
+ * @Laurel Woods 
+ * @10/28
+ */
+import java.util.Scanner;
+public class StudentList
+{
+    private Student[] students;
+
+    public StudentList()
+    {
+        students = new Student[3];
+    }
+
+    public void addStu(int stuIndex){
+        System.out.println("\n" +"*Adding new student*");
+        Scanner scr = new Scanner(System.in);
+        int id = 0;
+        double gpa = 0.0;
+        String name = "";
+        System.out.println("\n" +"Enter student name:");
+        name = scr.nextLine();
+        System.out.println("\n" +"Enter student ID number:");
+        id = scr.nextInt();
+        System.out.println("\n" +"Enter student GPA:");
+        gpa = scr.nextDouble();
+        String clearBuffer = scr. nextLine();
+        students[stuIndex] = new Student(id, gpa, name);
+    }
+    
+    
+}
